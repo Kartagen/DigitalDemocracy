70aa2defd1734904ce2003d9ef3fcba69ad2986a
EquesAuratus <illia.vysochyn@nure.ua>
2023-11-08 19:03:52 +0200

    Initial commit, users and database added

---
+++ index.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const mongoose = require("mongoose");
+const authRouter = require("./authRouter");
+const PORT = process.env.PORT || 5000;
+
+const app = express();
+const connection = "mongodb+srv://server:O8qy9c9vPJnxJJhd@cluster0.mmgb2cb.mongodb.net/?retryWrites=true&w=majority";
+
+app.use(express.json());
+app.use("/auth", authRouter);
+const start = async () =>{
+    try{
+        await mongoose.connect(connection);
+        app.listen(PORT, () => console.log(`server started at port ${PORT}`));
+    } catch (e){
+        console.log(e);
+    }
+}
+
+start();
